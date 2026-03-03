f63bbac2f6b8854fea9177731e6eb02331fe3050
shahid11-01 <shahid.ganbarov@gmail.com>
2026-03-03 18:02:50 +0900

    first step

---
+++ src/main/java/com/social/gatherly/GatherlyApplication.java
@@ -0,0 +1,13 @@
+package com.social.gatherly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatherlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatherlyApplication.class, args);
+    }
+
+}
